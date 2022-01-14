8e5e8542b476767734707fdb5a03ac8ac494be6c
vanhoan01 <92982928+vanhoan01@users.noreply.github.com>
2022-01-14 12:02:41 +0700

    Update thanhtoan.php

--- views/payment/thanhtoan.php
+++ views/payment/thanhtoan.php
@@ -3,5 +3,6 @@
     require_once('../../controllers/DonHangController.php');
     $controller_obj = new DonHangController();
     $controller_obj->thanhToan($idKH);
-    header("Location: ../../index.php");
+    echo "<script type='text/javascript'>alert('Đặt hàng thành công!');</script>";
+    header("Location: ../cart/cart.php");
 ?>
